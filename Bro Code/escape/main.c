1f165a9014fba2da4d06b27e2fc8e1516d41f5ae
LEON-AX <hichamohicham651@gmail.com>
2024-08-02 01:48:33 +0100

    learn C

---
+++ Bro Code/escape/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Hello\tWorld\n");
+
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+
+    printf("I Like \"GYM\"");
+    
+    printf("I Like \'Pc\'");
+
+    printf("\n");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+    respect of the environment. No obstacle can be moved to create the playground, be it
+    a tree, a stone, a road sign, a starship anchor, or, during a memorable match between
+    the Legally Administrated Vogons’ team and the Titans’ Titans team, an entire galactic
+    sector. The irony lying, of course, in the fate of the Galactic Sector ZZ9 Plural Z Alpha
+    shortly after the Vogons’ victory.
+
+
+*/
---
+++ Bro Code/variable/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int x;
+    x = 123;
+    int y = 255;
+
+    int age = 17;
+    float gpa =2.05;
+    char grade = 'C';
+    char name[] = "leon";
+
+    printf("You are age %i", age);
+    
+    printf("\n");
+}
